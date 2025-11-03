56abb9ab02a9afa7991e09e46aa000da39bf9dc4
Nhat <phantannhat12@gmail.com>
2025-11-03 15:04:19 +0700

    impl API /logout

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import { login, refreshToken } from '../controllers/auth';
+import { login, logout, refreshToken } from '../controllers/auth';
 import { validateLogin } from '../validations/login';
+import { authenticate } from '../middlewares/auth';
 
 const authRouter = express.Router();
 
 authRouter.post('/login', validateLogin, login);
 
 authRouter.post('/refresh-token', refreshToken);
+
+authRouter.post('/logout', authenticate, logout);
 
 export default authRouter;
